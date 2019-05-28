f9281933556e16728e32fa8d5038be67ad3f3a03
mery_cherrry@outlook.com <mery_cherrry@outlook.com>
2019-05-28 13:01:29 +0200

    adding product service

---
+++ src/app/shared/models/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+    key: string;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    imageUrl: string;
+}
---
+++ src/app/shared/services/products.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  create(product) {
+    return this.db.list('/products').push(product);
+  }
+
+  getAll() {
+    // we use snapshotchanges to map list and get also
+    // key parameter otherwise we would use commented line
+    const productRef = this.db.list('/products');
+    return productRef.snapshotChanges().pipe(map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val()}));
+    }));
+   // return this.db.list('/products').valueChanges();
+  }
+
+  get(productId) {
+    return this.db.object('/products/' + productId).valueChanges();
+  }
+
+  update(productId, product) {
+    return this.db.object('/products/' + productId).update(product);
+  }
+
+  delete(productId) {
+    return this.db.object('/products/' + productId).remove();
+  }
+}
