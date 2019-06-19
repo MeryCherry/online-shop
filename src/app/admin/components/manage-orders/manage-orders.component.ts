68c2d74453b1b07b2b02a31a3783e2af59ef110a
mery_cherrry@outlook.com <mery_cherrry@outlook.com>
2019-06-19 15:13:04 +0200

    change style sass file extension

--- src/app/admin/components/manage-orders/manage-orders.component.ts
+++ src/app/admin/components/manage-orders/manage-orders.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-manage-orders',
   templateUrl: './manage-orders.component.html',
-  styleUrls: ['./manage-orders.component.sass']
+  styleUrls: ['./manage-orders.component.scss']
 })
 export class ManageOrdersComponent implements OnInit {
 
