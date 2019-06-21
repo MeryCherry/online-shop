4399603f26e015d0623a00b5beff1bc4b167f6c0
mery_cherrry@outlook.com <mery_cherrry@outlook.com>
2019-06-21 17:44:03 +0200

    create shopping cart component and cart service

---
+++ src/app/shared/services/cart.service.ts
@@ -0,0 +1,101 @@
+import { ShoppingCart } from 'shared/models/shopping-cart';
+import { take, map } from 'rxjs/operators';
+import { Product } from 'shared/models/product';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  async getCart() {
+
+    let cartId = await this.getOrCreateCartId();
+    console.log(cartId);
+    let cart = this.db.object('/shopping-carts/' + cartId).snapshotChanges().pipe(
+    map((action: any) => {
+    const key = action.key;
+    const items = action.payload.val().items;
+    return new ShoppingCart(key, items);
+    }));
+    return cart;
+  }
+
+  async removeFromCart(product: Product) {
+    this.updateItem(product, -1 , 0);
+  }
+  async removeFromCartAllOfType(product: Product) {
+    // calling async method as sync method, with await
+    let cartId = await this.getOrCreateCartId();
+    let item = await this.getItem(cartId, product.key);
+    item.remove();
+  }
+  async addToCart(product: Product) {
+    this.updateItem(product, 1 , 1);
+  }
+
+  async clearCartItems() {
+    let cartId =  await this.getOrCreateCartId();
+    // remove all items from cart
+    this.db.object('/shopping-carts/' + cartId + '/items').remove();
+  }
+
+  create() {
+    return this.db.list('/shopping-carts').push({
+      dateCreated: new Date().getTime()
+    });
+  }
+
+  getItem( cartId: string, productId: string) {
+    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
+  }
+  private async getOrCreateCartId(): Promise<string> {
+        // to have possibility for creating and storring products
+    // in shopping cart for users that are not logged
+    // we use local storage to store data from shopping cart
+    // get id of shopping cart
+    let cartId = localStorage.getItem('cartID');
+    if (cartId) { return cartId; }
+
+      // create cart in database and set its key in local storage
+      // calling async method as sync method, with await
+    let result = await this.create();
+    localStorage.setItem('cartID', result.key);
+    return result.key;
+
+  }
+
+  // increase or decrease product quantity
+  // product => product to be added/removed
+  // change => number added to quantity of products
+  // minNum=> if there is no products in cart, what quantity should be set
+  private async updateItem(product: Product, change: number, minNum: number) {
+        // calling async method as sync method, with await
+        let cartId = await this.getOrCreateCartId();
+        // getting observable for shopping cart items
+        let item$ = this.getItem(cartId, product.key);
+       // if there is already item with id in shopping cart
+       // check property quantity, then update it
+       // otherwise add this property and set value to 1
+        item$.snapshotChanges()
+         .pipe(take(1))
+         .subscribe(i => {
+           let quantity = ((i.payload.hasChild('quantity')) ? (i.payload.val()['quantity' ] || 0 ) + change : minNum);
+           if (!quantity || quantity <= 0) {
+            item$.remove();
+           } else {
+            item$.update({
+              title: product.title,
+              price: product.price,
+              category: product.category,
+              imageUrl: product.imageUrl,
+              quantity: quantity
+            });
+           }
+         });
+  }
+
+}
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,26 @@
+import { Product } from './../shared/models/product';
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'shared/services/cart.service';
 
 @Component({
-  selector: 'app-shopping-cart',
+  selector: 'shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.sass']
+  styleUrls: ['./shopping-cart.component.scss']
 })
 export class ShoppingCartComponent implements OnInit {
+  cart$;
 
-  constructor() { }
+  constructor(private cartSrvc: CartService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.cart$ = await this.cartSrvc.getCart();
   }
 
+  clearCart() {
+    this.cartSrvc.clearCartItems();
+  }
+
+  removeAll(item: Product) {
+    this.cartSrvc.removeFromCartAllOfType(item);
+  }
 }
